0de5cbf3a2348fd55b242adf7a5ccb4263ff4874
Debasish Bhola <devop69@gmail.com>
2024-03-07 15:50:39 +0530

    Update scatter.py

--- scatter.py
+++ scatter.py
@@ -4,3 +4,5 @@ user = [70,65,72,63,71,64,69,64,67]
 minutes = [175,170,205,120,220,130,105,145,190]
 website = ['a','b','c','d','e','f','g','h','i']
 plt.scatter(user, minutes)
+for label,friend_count,minute_count in zip(website,user,minutes):
+    plt.annotate(label,xy=(friend_count,minute_count),xytext=(5,-5),textcords='offset point')

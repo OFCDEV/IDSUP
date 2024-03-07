12fcd2701e93c788a4c0b9eaeb1258692fe4ab8d
Debasish Bhola <devop69@gmail.com>
2024-03-07 14:39:48 +0530

    Update one.py

--- one.py
+++ one.py
@@ -2,4 +2,7 @@
 from matplotlib import pyplot
 years=[1950,1960,1980,1990,2000,2010]
 gdp=[300.2,543.3,1075.9,2062.5,5979.6,10289.7]
-pyplot.plot(years, gdp,color='red',marker='o')
+pyplot.plot(years, gdp,color='red',marker='o',linestyle='--')
+plt.title("Years and GDP")
+plt.xlabel('Years')
+plt.ylabel('GDP')

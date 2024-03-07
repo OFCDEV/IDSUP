92dfde5031eed4ff2deb2bbfd23710b40b86cd6c
Debasish Bhola <devop69@gmail.com>
2024-03-07 14:23:52 +0530

    Line Chart

---
+++ one.py
@@ -0,0 +1,5 @@
+'''Line chart'''
+from matplotlib import pyplot
+years=[1950,1960,1980,1990,2000,2010]
+gdp=[300.2,543.3,1075.9,2062.5,5979.6,10289.7]
+pyplot.plot(years, gdp,color='red',marker='o')
